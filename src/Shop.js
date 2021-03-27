6be6603f9facd38e718849f7ea1e8901805ecf7c
Amy Hua <aymay98@gmail.com>
2021-03-27 13:12:39 -0500

    Updated Shop

--- src/Shop.js
+++ src/Shop.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 function Shop() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const [items, setItems] = useState([]);
 
   const fetchItems = async () => {
-    const data = await fetch('')
+    const data = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get');
+
+    const items = await data.json();
+
+    console.log(items.data);
+    setItems(items.data);
   }
 
   return (
-    <div className="Shop">
-      <h2>Shop page</h2>
+    <div>
+       {items.map(item =>(
+         <h1 key={item.itemid}>{item.name}</h1>
+       ))}
     </div>
   );
 }
